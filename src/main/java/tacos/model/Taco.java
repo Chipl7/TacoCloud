ce2932f341e6a2daf5500b2039e16bc9611e8faa
Artem <artem.chipl@gmail.com>
2024-12-16 16:01:10 +0700

    Taco and TacoOrder update

--- src/main/java/tacos/model/Taco.java
+++ src/main/java/tacos/model/Taco.java
@@ -18,7 +18,7 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
     @Size(min = 1, message = "You must choose ad least 1 ingredient")
     private List<Ingredient> ingredients;
+
 }
